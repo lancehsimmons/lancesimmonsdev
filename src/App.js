ba5d780ddecbeb13a802e64c7032583f4d2b5ed7
Lance Simmons <lancehowardsimmons@gmail.com>
2021-12-22 17:40:02 -0500

    full-stack svg and web dev title added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import './App.css';
+import { ReactComponent as FullStack } from './assets/full_stack.svg'
 
 function App() {
   return (
     <div className="App">
+      <div>
+        <h1 className="title">WEB<br/>DEV</h1>
+        <FullStack className='full-stack'/>
+      </div>
     </div>
   );
 }
