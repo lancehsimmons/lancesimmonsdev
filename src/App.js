bb8b6f98deede6f421871f0f4539f80b14719133
Lance Simmons <lancehowardsimmons@gmail.com>
2021-12-23 11:41:51 -0500

    adds a href to external links

--- src/App.js
+++ src/App.js
@@ -35,24 +35,36 @@ function App() {
           </div>
           <div className='screens'>
             <div className='resume'>
-            <h3>
-              <span className='blue'>RESUME</span>
-            </h3>
+              <h3>
+                <span className='blue'>RESUME</span>
+              </h3>
             </div>
             <div>
-            <h3>
-              <span className='red'>PORTFOLIO</span>
-            </h3>
+              <h3>
+                <span className='red'>PORTFOLIO</span>
+              </h3>
             </div>
           </div>
         </div>
         <div className='statement'>
-            <p className='blue'>  As a <span className='red'>software engineer</span> with a background in a diverse <span className='red'>art</span> practice and a decade of technical support I seek to bring my <span className='red'>passion</span> for sharing in <span className='red'>creativity</span> to bear on every aspect of my life.</p>
+          <p className='blue'>  As a <span className='red'>software engineer</span> with a background in a diverse <span className='red'>art</span> practice and a decade of technical support I seek to bring my <span className='red'>passion</span> for sharing in <span className='red'>creativity</span> to bear on every aspect of my life.</p>
         </div>
         <div className='links'>
-          <div><img src='./images/mail.png'/></div>
-          <div><img src='./images/github.png'/></div>
-          <div><img src='./images/linked.png'/></div>
+          <div>
+            <a href='mailto:lancehowardsimmons@gmail.com'>
+              <img src='./images/mail.png' />
+            </a>
+          </div>
+          <div>
+            <a href='https://github.com/lancehsimmons'>
+              <img src='./images/github.png' />
+            </a>
+          </div>
+          <div>
+            <a href='https://www.linkedin.com/in/lance-simmons/'>
+              <img src='./images/linked.png' />
+            </a>
+          </div>
         </div>
       </div>
     </div>
