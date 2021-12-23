4fa6561bd521f2f7c1ad838268dfff71b54ca0aa
Lance Simmons <lancehowardsimmons@gmail.com>
2021-12-23 14:57:19 -0500

    portfolio piece cards in place with github links

--- src/App.js
+++ src/App.js
@@ -3,84 +3,79 @@ import Portfolio from './screens/Portfolio'
 import './App.css';
 import { ReactComponent as FullStack } from './assets/Group 77.svg'
 
-// import './assets/mail.png'
-// import './assets/github.png'
-// import './assets/linked.png'
-
 function App() {
   return (
     <div className="App">
-
       <Switch>
-        <Route path='/'></Route>
-        <Route path='/portfolio'><Portfolio/></Route>
+        <Route path='/portfolio'><Portfolio /></Route>
+        <Route path='/lancedev'exact>
+          <div className='title-1-div column1'>
+            <FullStack className='full-stack' />
+          </div>
+          <div className='content-div column'>
+            <div className='lance-div'>
+              <h1 className="lance">
+                <span className='blue'>LANCE</span>
+                <span className='red'> SIMMONS</span>
+              </h1>
+            </div>
+            <div className='sub-title'>
+              <div className='fs-container'>
+                <div>
+                  <h2 className='full-stack-title'>
+                    <span className='white'> {` {{{`}</span>
+                    <span className='red'>FULL</span>
+                  </h2>
+                </div>
+                <h2 className='full-stack-title'>
+                  <span className='blue'>STACK</span>
+                </h2>
+              </div>
+              <div className='slash'>
+                <span className='white'>/</span>
+              </div>
+              <div className='screens'>
+                <div className='resume'>
+                  <h3>
+                    <span className='blue'>
+                      <a href='https://docs.google.com/document/d/1s_NQsBNisr4WyLuihdOOqQxq78PrDvwjx_WpIe3OR8o/edit?usp=sharing'>RESUME</a>
+                    </span>
+                  </h3>
+                </div>
+                <div>
+                  <h3>
+                    <Link to='/portfolio'>
+                      <span className='red'>
+                        PORTFOLIO
+                      </span>
+                    </Link>
+                  </h3>
+                </div>
+              </div>
+            </div>
+            <div className='statement'>
+              <p className='blue'>  As a <span className='red'>software engineer</span> with a background in a diverse <span className='red'>art</span> practice and a decade of technical support I seek to bring my <span className='red'>passion</span> for sharing in <span className='red'>creativity</span> to bear on every aspect of my life.</p>
+            </div>
+            <div className='links'>
+              <div>
+                <a href='mailto:lancehowardsimmons@gmail.com'>
+                  <img src='./images/mail.png' />
+                </a>
+              </div>
+              <div>
+                <a href='https://github.com/lancehsimmons'>
+                  <img src='./images/github.png' />
+                </a>
+              </div>
+              <div>
+                <a href='https://www.linkedin.com/in/lance-simmons/'>
+                  <img src='./images/linked.png' />
+                </a>
+              </div>
+            </div>
+          </div>
+        </Route>
       </Switch>
-
-      <div className='title-1-div column1'>
-        <FullStack className='full-stack' />
-      </div>
-      <div className='content-div column'>
-        <div className='lance-div'>
-          <h1 className="lance">
-            <span className='blue'>LANCE</span>
-            <span className='red'> SIMMONS</span>
-          </h1>
-        </div>
-        <div className='sub-title'>
-          <div className='fs-container'>
-            <div>
-              <h2 className='full-stack-title'>
-                <span className='white'> {` {{{`}</span>
-                <span className='red'>FULL</span>
-              </h2>
-            </div>
-            <h2 className='full-stack-title'>
-              <span className='blue'>STACK</span>
-            </h2>
-          </div>
-          <div className='slash'>
-            <span className='white'>/</span>
-          </div>
-          <div className='screens'>
-            <div className='resume'>
-              <h3>
-                <span className='blue'>
-                  <a href='https://docs.google.com/document/d/1s_NQsBNisr4WyLuihdOOqQxq78PrDvwjx_WpIe3OR8o/edit?usp=sharing'>RESUME</a>
-                </span>
-              </h3>
-            </div>
-            <div>
-              <h3>
-                <Link to='/portfolio'>
-                  <span className='red'>
-                    PORTFOLIO
-                  </span>
-                </Link>
-              </h3>
-            </div>
-          </div>
-        </div>
-        <div className='statement'>
-          <p className='blue'>  As a <span className='red'>software engineer</span> with a background in a diverse <span className='red'>art</span> practice and a decade of technical support I seek to bring my <span className='red'>passion</span> for sharing in <span className='red'>creativity</span> to bear on every aspect of my life.</p>
-        </div>
-        <div className='links'>
-          <div>
-            <a href='mailto:lancehowardsimmons@gmail.com'>
-              <img src='./images/mail.png' />
-            </a>
-          </div>
-          <div>
-            <a href='https://github.com/lancehsimmons'>
-              <img src='./images/github.png' />
-            </a>
-          </div>
-          <div>
-            <a href='https://www.linkedin.com/in/lance-simmons/'>
-              <img src='./images/linked.png' />
-            </a>
-          </div>
-        </div>
-      </div>
     </div>
   );
 }
--- src/screens/Portfolio.jsx
+++ src/screens/Portfolio.jsx
@@ -1,7 +1,49 @@
+import './portfolio.css'
+
 export default function Portfolio() {
   return (
     <div>
-      <h1>PORTFOLIO</h1>
+      <div className='piece'>
+        <div className='piece-title-div'>
+        <img className='thumb'
+            src='./images/roast-thumbnail.png' />
+          <h2 className='red piece-title'>ROAST</h2>
+          <a href='https://github.com/lancehsimmons/Roast'>
+            <img className='github'
+              src='./images/github.png' />
+          </a>
+        </div>
+        <p className='blue'>  Roast is a dynamic, precision home coffee roasting log. A full-stack web application providing full CRUD, built on Ruby on Rails and React. Users can create and edit log entries for their latest coffee roasts. Perfecting roasts on your favorite beans is a laborious process. I've created Roast to give home-roasters like myself the tools they need to achieve the ideal bean.
+        </p>
+      </div>
+
+      <div className='piece'>
+        <div className='piece-title-div'>
+          <img className='thumb'
+            src='./images/everfree-thumbnail.png' />
+          <h2 className='red piece-title'>EVERFREE OUTDOORS</h2>
+          <a href='https://github.com/lancehsimmons/HEED'>
+            <img className='github'
+              src='./images/github.png' />
+          </a>
+        </div>
+        <p className='blue'>  Everfree Outdoors is a fully functioning CRUD full-stack prototype for an e-commerce site. The fauxe company Everfree brings together a team of developers to provide a one-stop shop for gearing up for new adventures. The focus of this project was to produce a full-stack app built on the MERN stack as a team. Coming together to share creativity and skills was a true pleasure.
+        </p>
+      </div>
+
+      <div className='piece'>
+        <div className='piece-title-div'>
+        <img className='thumb'
+            src='./images/heed-thumbnail.png' />
+          <h2 className='red piece-title'>HEED</h2>
+          <a href='https://github.com/anelisekathryn/EverFree-Outdoors'>
+            <img className='github'
+              src='./images/github.png' />
+          </a>
+        </div>
+        <p className='blue'>  An early warning Asteroid Cataclysm Tracker. HEED consumes active data from the Near Earth Object Web Service NASA api and presents select information in a playful and engaging interface. Built on raw HTML, CSS, and Javascript, HEED was conceived of as an exercise in basic front-end api functionallity.
+        </p>
+      </div>
     </div>
   )
 }
