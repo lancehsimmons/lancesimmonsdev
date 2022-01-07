436e75c440d9a324df7b490ac40799ab56aea274
Lance Simmons <lancehowardsimmons@gmail.com>
2022-01-07 12:56:56 -0500

    porfolio piece title styling

--- src/screens/Portfolio.jsx
+++ src/screens/Portfolio.jsx
@@ -15,6 +15,7 @@ export default function Portfolio() {
             <img className='thumb'
               src='./images/roast-thumbnail.png' alt='roast app logo'/>
           </a>
+          <div>
           <h2 className='red piece-title'>ROAST</h2>
           <a target="_blank"
             rel="noopener noreferrer"
@@ -22,6 +23,7 @@ export default function Portfolio() {
             <img className='github'
               src='./images/github.png' alt='github logo' />
           </a>
+          </div>
         </div>
         <p className='blue'>  Roast is a dynamic, precision home coffee roasting log. A full-stack web application providing full CRUD, built on Ruby on Rails and React. Users can create and edit log entries for their latest coffee roasts. Perfecting roasts on your favorite beans is a laborious process. I've created Roast to give home-roasters like myself the tools they need to achieve the ideal bean.
         </p>
@@ -35,6 +37,7 @@ export default function Portfolio() {
             <img className='thumb'
               src='./images/everfree-thumbnail.png' alt='everfree site screen shot'/>
           </a>
+            <div>
           <h2 className='red piece-title'>EVERFREE OUTDOORS</h2>
           <a target="_blank"
             rel="noopener noreferrer"
@@ -42,6 +45,7 @@ export default function Portfolio() {
             <img className='github'
               src='./images/github.png' alt='github logo'/>
           </a>
+          </div>
         </div>
         <p className='blue'>  Everfree Outdoors is a fully functioning CRUD full-stack prototype for an e-commerce site. The fauxe company Everfree brings together a team of developers to provide a one-stop shop for gearing up for new adventures. The focus of this project was to produce a full-stack app built on the MERN stack as a team. Coming together to share creativity and skills was a true pleasure.
         </p>
@@ -55,6 +59,8 @@ export default function Portfolio() {
             <img className='thumb'
               src='./images/heed-thumbnail.png' alt='heed app screen shot'/>
           </a>
+          <div>
+
           <h2 className='red piece-title'>HEED</h2>
           <a target="_blank"
             rel="noopener noreferrer"
@@ -62,6 +68,7 @@ export default function Portfolio() {
             <img className='github'
               src='./images/github.png' alt='github logo'/>
           </a>
+          </div>
         </div>
         <p className='blue'>  An early warning Asteroid Cataclysm Tracker. HEED consumes active data from the Near Earth Object Web Service NASA api and presents select information in a playful and engaging interface. Built on raw HTML, CSS, and Javascript, HEED was conceived of as an exercise in basic front-end api functionallity.
         </p>
